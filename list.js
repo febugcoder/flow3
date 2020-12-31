07279bc8d8a6435df89ac962ddc53713e9ccbc6a
copy <copy.com>
2020-12-31 11:10:12 +0800

    master merge

--- list.js
+++ list.js
@@ -4,12 +4,6 @@ console.log('master up')
 console.log(`1`)
 console.log(`2`)
 console.log(`3`)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 console.log(`4`)
->>>>>>> bcbc392 (4)
-=======
 console.log(`4`)
 console.log(`5`)
->>>>>>> d71406b (5)
